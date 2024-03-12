98e2ede24406ecfe13565c0327702af4efa1e7be
dague <daniel.guedj.pro@gmail.com>
2024-03-12 16:46:34 +0100

    ADD: basic quiery

--- FrontEnd/javascript/test.js
+++ FrontEnd/javascript/test.js
@@ -1,6 +1,19 @@
-// URL de l'API Swagger
+const reponse = await fetch('http://localhost:5678/api/works');
+const works = await reponse.json();
 
-// Récupération des pièces depuis l'API
-const reponse = await fetch('http://localhost:5678/api/users');
-let users = await reponse.json();
-const valeurusers = JSON.stringify(users);
+function genererWork(works) {
+
+    const article = works[0];
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".test_class");
+    // Création d’une balise dédiée à une pièce automobile
+    const worksElements = document.createElement("article");
+    worksElements.dataset.id = article.id;
+    worksElements.dataset.title = article.title;
+    const imageElements = document.createElement("img");
+    imageElements.src = article.imageUrl;
+    sectionFiches.appendChild(worksElements);
+    worksElements.appendChild(imageElements);
+
+}
+genererWork(works);
