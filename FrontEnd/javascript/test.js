8b9d1d3e750ae627805fb82eb82a040324a8050d
dague <daniel.guedj.pro@gmail.com>
2024-03-13 10:46:20 +0100

    ADD: BDD collected articles

--- FrontEnd/javascript/test.js
+++ FrontEnd/javascript/test.js
@@ -3,17 +3,22 @@ const works = await reponse.json();
 
 function genererWork(works) {
 
-    const article = works[0];
+    for (let i = 0; i < works.length; i++) {
+
+    const article = works[i];
     // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionFiches = document.querySelector(".test_class");
+    const sectionFiches = document.querySelector(".gallery");
     // Création d’une balise dédiée à une pièce automobile
-    const worksElements = document.createElement("article");
-    worksElements.dataset.id = article.id;
-    worksElements.dataset.title = article.title;
+    const worksElements = document.createElement("figure");
     const imageElements = document.createElement("img");
+    const figcaptionElements = document.createElement("figcaption")
     imageElements.src = article.imageUrl;
+    imageElements.alt = article.title;
+    figcaptionElements.innerText = article.title;
     sectionFiches.appendChild(worksElements);
     worksElements.appendChild(imageElements);
+    worksElements.appendChild(figcaptionElements);
+    }
 
 }
 genererWork(works);
