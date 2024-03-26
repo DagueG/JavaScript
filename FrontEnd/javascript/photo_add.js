5188bb379e40d93bd49d3c37ea0f899aad55fd2e
dague <daniel.guedj.pro@gmail.com>
2024-03-26 15:52:50 +0100

    fix: style

--- FrontEnd/javascript/photo_add.js
+++ FrontEnd/javascript/photo_add.js
@@ -4,15 +4,23 @@ export function showAddPhotoModal() {
 
     const modalImage = document.querySelector('.image-modal');
 
-    var backButton = document.createElement('img');
-    backButton.classList.add('back-button')
-    backButton.src = './assets/icons/arrow-left-solid.svg';
+    var backButton = document.createElement('button');
+    backButton.classList.add('back-button');
+
+    // Créer un élément <span> pour le symbole de la flèche
+    var arrowSpan = document.createElement('img');
+    arrowSpan.src = './assets/icons/arrow-left-solid.svg';
+
+    backButton.appendChild(arrowSpan);
+
+    // Écouteur d'événement sur le bouton
     backButton.onclick = function() {
         // Revenir à la modal précédente
         document.querySelector('.overlay').remove();
         modalImage.remove();
         showImageModal(works);
     };
-    modalImage.appendChild(backButton);
 
+    // Insérer le bouton avant le premier enfant de la modal
+    modalImage.insertBefore(backButton, modalImage.firstChild);
 }
